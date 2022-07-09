f55518be6283e7fa6bf0e072cb58debfef6d86b1
lucas <lucassenazuza@gmail.com>
2022-07-09 18:13:47 -0300

    lucas

--- video_stream.py
+++ video_stream.py
@@ -1,34 +1,30 @@
-from flask import Flask, render_template, Response
 import cv2
+import numpy
+from flask import Flask, render_template, Response, stream_with_context, request
 
-app = Flask(__name__)
-camera = cv2.VideoCapture(0)
+video = cv2.VideoCapture(0)
+app = Flask('__name__')
 
 
-def generate_frames():
+def video_stream():
     while True:
-
-        ## read the camera frame
-        success, frame = camera.read()
-        if not success:
-            break
+        ret, frame = video.read()
+        if not ret:
+            break;
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
+            ret, buffer = cv2.imencode('.jpeg',frame)
             frame = buffer.tobytes()
-
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            yield (b' --frame\r\n' b'Content-type: imgae/jpeg\r\n\r\n' + frame +b'\r\n')
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/camera')
+def camera():
+    return render_template('camera.html')
 
 
-@app.route('/stream')
-def video():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    return Response(video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port='8080')
+app.run(host='0.0.0.0', port='5000', debug=False)
